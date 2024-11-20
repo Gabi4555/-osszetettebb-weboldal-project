d769223be459b6538cb2aef828337b727169e3a4
Gabi455 <gabi455@yahoo.com>
2024-11-20 21:17:16 +0100

    na meg van ok

--- javascript/mobs.js
+++ javascript/mobs.js
@@ -7,9 +7,11 @@ function clickhandler(mob) {
           fetch('../jsons/skeleton.json')
                 .then(response => response.json())
                 .then(mob_data => { 
-                  document.querySelector("#health").innerHTML = "FUCK"
-
-                
+                  document.querySelector("#health").innerHTML = mob_data.health
+                  document.querySelector("#damage").innerHTML = mob_data.damage
+                  document.querySelector("#other").innerHTML = mob_data.other
+                  document.querySelector("#behivor").innerHTML = mob_data.behivor
+                  document.querySelector("#mod_image").style = mob_data.image
                 })
           break;
         case 'Zombie':
