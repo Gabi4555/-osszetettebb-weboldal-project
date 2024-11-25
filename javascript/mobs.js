aca90e912f618d390cc6291dac2b070826864960
Gabi455 <gabi455@yahoo.com>
2024-11-25 20:01:37 +0100

    a mobok nagyából kész

--- javascript/mobs.js
+++ javascript/mobs.js
@@ -62,14 +62,52 @@ function clickhandler(mob) {
 
 
           break;
-          case 'Creeper':
-          
+          case 'Cow':
+            fetch('../jsons/cow.json')
+            .then(response => response.json())
+            .then(mob_data => { 
+              document.querySelector("#health").innerHTML = mob_data.health
+              document.querySelector("#damage").innerHTML = mob_data.damage
+              document.querySelector("#other").innerHTML = mob_data.other
+              document.querySelector("#behivor").innerHTML = mob_data.behivor
+              document.querySelector("#mod_image").style = mob_data.image
+              document.querySelector("#mob_name").innerHTML = mob_data.name
+              document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+            })
 
 
 
           break;
-          case 'Creeper':
-          
+          case 'Lamb':
+            fetch('../jsons/lamb.json')
+            .then(response => response.json())
+            .then(mob_data => { 
+              document.querySelector("#health").innerHTML = mob_data.health
+              document.querySelector("#damage").innerHTML = mob_data.damage
+              document.querySelector("#other").innerHTML = mob_data.other
+              document.querySelector("#behivor").innerHTML = mob_data.behivor
+              document.querySelector("#mod_image").style = mob_data.image
+              document.querySelector("#mob_name").innerHTML = mob_data.name
+              document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+            })
+
+
+
+
+          break;
+          case 'Pig':
+            fetch('../jsons/pig.json')
+            .then(response => response.json())
+            .then(mob_data => { 
+              document.querySelector("#health").innerHTML = mob_data.health
+              document.querySelector("#damage").innerHTML = mob_data.damage
+              document.querySelector("#other").innerHTML = mob_data.other
+              document.querySelector("#behivor").innerHTML = mob_data.behivor
+              document.querySelector("#mod_image").style = mob_data.image
+              document.querySelector("#mob_name").innerHTML = mob_data.name
+              document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+            })
+
 
 
 
