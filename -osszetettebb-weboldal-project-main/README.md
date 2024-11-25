44a1c67169caa47114a1b1cc3b5e68ef7ab485c0
istván <113088714+Istvan0213@users.noreply.github.com>
2024-11-25 19:35:08 +0100

    ez a több JSON

---
+++ -osszetettebb-weboldal-project-main/README.md
@@ -0,0 +1,1 @@
+# -osszetettebb-weboldal-project
---
+++ -osszetettebb-weboldal-project-main/javascript/tips.js
@@ -0,0 +1,37 @@
+
+
+let random_szam = 1
+let elobbi_szam = 1
+let mas = false
+function clickhandler() {
+    mas = false
+while (mas == false) {
+    random_szam = Math.floor(Math.random() * 5) + 1;
+    if (random_szam != elobbi_szam) {
+         mas = true
+         elobbi_szam = random_szam
+
+
+
+         
+    }
+}
+
+
+
+
+fetch('../jsons/tips.json')
+.then(response => response.json())
+                .then(tips => { 
+                    document.querySelector("#tippek").innerHTML =  tips['tip_' + random_szam]
+                    
+
+
+
+
+
+                })
+
+
+
+}
