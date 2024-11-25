018520df06ca4caa0eed59a3d1c7f7563c41460c
Gabi455 <gabi455@yahoo.com>
2024-11-25 19:54:02 +0100

    se

--- javascript/mobs.js
+++ javascript/mobs.js
@@ -17,7 +17,62 @@ function clickhandler(mob) {
                 })
           break;
         case 'Zombie':
-          // code block
+          fetch('../jsons/zombie.json')
+          .then(response => response.json())
+          .then(mob_data => { 
+            document.querySelector("#health").innerHTML = mob_data.health
+            document.querySelector("#damage").innerHTML = mob_data.damage
+            document.querySelector("#other").innerHTML = mob_data.other
+            document.querySelector("#behivor").innerHTML = mob_data.behivor
+            document.querySelector("#mod_image").style = mob_data.image
+            document.querySelector("#mob_name").innerHTML = mob_data.name
+            document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+          })
+
+          break;
+          case 'Creeper':
+            fetch('../jsons/creeper.json')
+          .then(response => response.json())
+          .then(mob_data => { 
+            document.querySelector("#health").innerHTML = mob_data.health
+            document.querySelector("#damage").innerHTML = mob_data.damage
+            document.querySelector("#other").innerHTML = mob_data.other
+            document.querySelector("#behivor").innerHTML = mob_data.behivor
+            document.querySelector("#mod_image").style = mob_data.image
+            document.querySelector("#mob_name").innerHTML = mob_data.name
+            document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+          })
+
+
+
+          break;
+          case 'Spider':
+            fetch('../jsons/spider.json')
+            .then(response => response.json())
+            .then(mob_data => { 
+              document.querySelector("#health").innerHTML = mob_data.health
+              document.querySelector("#damage").innerHTML = mob_data.damage
+              document.querySelector("#other").innerHTML = mob_data.other
+              document.querySelector("#behivor").innerHTML = mob_data.behivor
+              document.querySelector("#mod_image").style = mob_data.image
+              document.querySelector("#mob_name").innerHTML = mob_data.name
+              document.querySelector("#mob_descripton").innerHTML = mob_data.descripton
+            })
+
+
+
+          break;
+          case 'Creeper':
+          
+
+
+
+          break;
+          case 'Creeper':
+          
+
+
+
           break;
         default:
           alert("Mob error")
