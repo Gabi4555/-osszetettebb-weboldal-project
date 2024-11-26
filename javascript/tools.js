19d1d7cdffab98c6179b82353ce533a73f5f21ad
Gabi455 <gabi455@yahoo.com>
2024-11-26 21:27:10 +0100

    sex

--- javascript/tools.js
+++ javascript/tools.js
@@ -92,7 +92,18 @@ function clickhandler(tool) {
 
           break;
           case 'Shovel':
-           
+            fetch('../jsons/shovel.json')
+            .then(response => response.json())
+            .then(tool_data => { 
+              
+              document.querySelector("#name").innerHTML = tool_data.name
+              document.querySelector("#durability").innerHTML = tool_data[material + '_durability']
+              document.querySelector("#speed").innerHTML = tool_data[material + '_speed']
+              document.querySelector("#other").innerHTML = tool_data[material + '_other']
+              document.querySelector("#tool_descripton").innerHTML = tool_data.descripton
+              document.querySelector("#mod_image").style = tool_data[material + '_image']
+              document.querySelector("#speed_info").innerHTML = tool_data.speed_explonation
+            })
 
           break;
         default:
